5a9797afe08f8cc003c5a581a775cbc51b1f0564
GabrielRefundini <52221116+GabrielRefundini@users.noreply.github.com>
2019-06-25 18:51:54 -0300

    Traduzi o hello world

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Hello WORLD, <strong>EU NASCI! </strong> DEDICAÇÃO nessa porra! 33h em 1 semana boraaaa!!";
+echo "Ola mundo querido, <strong>EU NASCI! </strong> DEDICAÇÃO nessa porra! 33h em 1 semana boraaaa!!";
 
 ?>
